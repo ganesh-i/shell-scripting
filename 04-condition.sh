aead1fdde32280db7607897d1a7bca72758d9e95
ganeshisukapalli0 <ganeshisukapalli0@gmail.com>
2026-01-29 17:49:39 +0530

    nos cal

--- 04-condition.sh
+++ 04-condition.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 num=$1
 if [ $num  -gt 20 ]; then 
- echo "given number: $num greater than 20"
+ echo "given number: $num is greater than 20"
 else
-echo "given number: $sum  is less than 20"
+echo "given number: $num  is less than 20"
 fi
